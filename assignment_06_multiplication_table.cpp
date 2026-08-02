3d17c7cac27e4eb232cfd23d983a00a125e0074d
via-efo <ajdzidzorm@gmail.com>
2026-08-02 00:58:58 +0000

    Complete Assignment 6: Multiplication Table

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -57,3 +57,47 @@
 #include <iostream>
 using namespace std;
 
+void printTable(int num)
+{
+    cout << "Multiplication Table for " << num << ":" << endl;
+    for (int i = 1; i <= 12; i++)
+    {
+        cout << num << " x " << i << " = " << (num * i) << endl;
+    }
+}
+
+void printTablesUpTo(int n)
+{
+    for (int num = 1; num <= n; num++)
+    {
+        printTable(num);
+        if (num != n)
+        {
+            cout << "---------------------------" << endl;
+        }
+    }
+}
+
+int main()
+{
+    cout << "--- Part A: Single Table ---" << endl;
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    printTable(number);
+
+    cout << endl
+         << "--- Part B: Tables from 1 to N ---" << endl;
+    int n;
+    cout << "Enter N: ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Error: N must be a positive integer." << endl;
+        return 0;
+    }
+    printTablesUpTo(n);
+
+    return 0;
+}
